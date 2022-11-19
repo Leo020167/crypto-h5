0b40b4c4f3968c2b5f7398ce8ac042f1514bfbcf
Lucasllin <2293560686@qq.com>
2022-11-19 14:56:27 +0800

    社區

--- src/api/mutator/custom-instance.ts
+++ src/api/mutator/custom-instance.ts
@@ -1,10 +1,24 @@
 import Axios, { AxiosError, AxiosRequestConfig } from 'axios';
+import { signParameters } from '../../utils/signature';
 
 export const AXIOS_INSTANCE = Axios.create({ baseURL: '/procoin' });
 
 export const customInstance = <T>(config: AxiosRequestConfig): Promise<T> => {
   const source = Axios.CancelToken.source();
-  const promise = AXIOS_INSTANCE({ ...config, cancelToken: source.token }).then(({ data }) => data);
+
+  const formData = new FormData();
+  const json = signParameters(config.data);
+
+  for (const i in json) {
+    formData.append(i, json[i]);
+  }
+
+  const promise = AXIOS_INSTANCE({
+    ...config,
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    data: formData,
+    cancelToken: source.token,
+  }).then(({ data }) => data);
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
