5bb1e5e2f6774cbc2846fc2aa43be35b8cc64307
Lucasllin <2293560686@qq.com>
2023-01-18 23:25:18 +0800

    优化

--- src/pages/HomeMarket/HomeStockDigitalMarket.tsx
+++ src/pages/HomeMarket/HomeStockDigitalMarket.tsx
@@ -1,13 +1,34 @@
-import { List } from 'antd-mobile';
+import { useTrail, animated } from '@react-spring/web';
+import { ErrorBlock, SpinLoading } from 'antd-mobile';
+import { useAtom } from 'jotai';
 import { stringify } from 'query-string';
+import { useCallback, useMemo } from 'react';
 import { useIntl } from 'react-intl';
 import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
+import { switchColorValueAtom } from '../../atoms';
 import { useMarketData } from '../../market/endpoints/marketWithTransformer';
 import HomeMarketItem from './HomeMarketItem';
 
 const HomeStockDigitalMarket = ({ tab }: { tab: string }) => {
-  const { data } = useMarketData({
+  const [upDownColor] = useAtom(switchColorValueAtom);
+
+  const getBackgroundColor = useCallback(
+    (rate: number) => {
+      if (upDownColor === '0') {
+        if (rate > 0) return 'rgba(226, 33, 78, 0.65)';
+        else if (rate === 0 || rate === -100) return '#555555';
+        return 'rgba(0, 173, 136, 0.65)';
+      } else {
+        if (rate > 0) return 'rgba(0, 173, 136, 0.65)';
+        else if (rate === 0 || rate === -100) return '#555555';
+        return 'rgba(226, 33, 78, 0.65)';
+      }
+    },
+    [upDownColor],
+  );
+
+  const { data, isLoading } = useMarketData({
     sortField: '',
     sortType: '',
     tab: tab,
@@ -16,6 +37,39 @@ const HomeStockDigitalMarket = ({ tab }: { tab: string }) => {
   const history = useHistory();
 
   const intl = useIntl();
+
+  const quotes = useMemo(() => data?.data?.quotes ?? [], [data?.data?.quotes]);
+
+  const [trails] = useTrail(
+    quotes.length,
+    (i) => {
+      return {
+        from: { opacity: 1 },
+        to: { opacity: 0 },
+        delay: i % 3 === 0 ? i * 100 : i % 2 === 0 ? i * 180 : i * 140,
+        config: {
+          mass: 3,
+        },
+      };
+    },
+    [],
+  );
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-full">
+        <SpinLoading color="primary" />
+      </div>
+    );
+  }
+
+  if (data?.data?.quotes?.length === 0) {
+    return (
+      <div className="flex items-center justify-center h-full">
+        <ErrorBlock status="empty" />
+      </div>
+    );
+  }
 
   return (
     <Container className="h-full flex flex-col text-xs">
@@ -30,53 +84,48 @@ const HomeStockDigitalMarket = ({ tab }: { tab: string }) => {
           <span>{intl.formatMessage({ defaultMessage: '漲跌幅', id: 'gA15gF' })}</span>
         </div>
       </div>
-      <div className="flex-1 overflow-y-auto">
-        <List>
-          {data?.data?.quotes?.map((v, i) => (
-            <List.Item
-              key={i}
-              onClick={() => {
-                if (tab === 'spot') {
-                  history.push({
-                    pathname: '/market2',
-                    search: stringify({
-                      symbol: v.symbol,
-                      isLever: 1,
-                    }),
-                  });
-                } else {
-                  history.push({
-                    pathname: '/market',
-                    search: stringify({
-                      symbol: v.symbol,
-                      isLever: 1,
-                      accountType: tab,
-                    }),
-                  });
-                }
-              }}
-              arrow={null}
-            >
-              <HomeMarketItem data={v} />
-            </List.Item>
-          ))}
-        </List>
+      <div className="flex-1 overflow-y-auto list">
+        {trails.map(({ opacity }, i) => (
+          <animated.a
+            className="relative"
+            key={i}
+            onClick={() => {
+              if (tab === 'spot') {
+                history.push({
+                  pathname: '/market2',
+                  search: stringify({
+                    symbol: quotes[i].symbol,
+                    isLever: 1,
+                  }),
+                });
+              } else {
+                history.push({
+                  pathname: '/market',
+                  search: stringify({
+                    symbol: quotes[i].symbol,
+                    isLever: 1,
+                    accountType: tab,
+                  }),
+                });
+              }
+            }}
+          >
+            <animated.div
+              className="absolute top-0 right-0 bottom-0 left-0 z-10"
+              style={{ backgroundColor: getBackgroundColor(Number(quotes[i].rate)), opacity }}
+            ></animated.div>
+            <HomeMarketItem data={quotes[i]} />
+          </animated.a>
+        ))}
       </div>
     </Container>
   );
 };
 
 const Container = styled.div`
-  .adm-list {
-    --padding-left: 0;
-    --padding-right: 0;
-    --border-inner: 0;
-    --border-top: 0;
-    --border-bottom: 0;
-
-    .adm-list-item:nth-child(odd) {
-      background-color: #f6f7f8;
-    }
+  .list a {
+    display: block;
+    padding: 12px 0;
   }
 `;
 
