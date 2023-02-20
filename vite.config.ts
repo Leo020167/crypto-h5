805287ef61fb7c0eec375db35e2c3994ea554a4d
Lucasllin <2293560686@qq.com>
2023-02-20 22:46:29 +0800

    優化打包

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'dist/TradingView',
+    outDir: 'dist/TradingView-H5',
   },
   server: {
     proxy: {
