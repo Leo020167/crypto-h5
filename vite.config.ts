fdc72467f7959100e5e61778383a41583724e256
Lucasllin <2293560686@qq.com>
2023-03-05 18:04:27 +0800

    優化

--- vite.config.ts
+++ vite.config.ts
@@ -7,47 +7,49 @@ import vitePluginImp from 'vite-plugin-imp';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    outDir: 'dist/Encrypted',
-  },
-  server: {
-    proxy: {
-      // http://api.piglobalexchanges.com/procoin/swagger/index.html#/
-      // http://api.worldcoinservice.com/procoin/swagger/index.html#/
-      '/procoin-market': {
-        target: 'http://market.encryptedex.com',
-        changeOrigin: true,
-      },
-      '/procoin-file': {
-        target: 'http://upload.encryptedex.com',
-        changeOrigin: true,
-      },
-      '/procoin': {
-        target: 'http://api.encryptedex.com',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  return {
+    build: {
+      outDir: `dist/Encrypted-${mode === 'allow-download' ? '有下載' : '無下載'}`,
+    },
+    server: {
+      proxy: {
+        // http://api.piglobalexchanges.com/procoin/swagger/index.html#/
+        // http://api.worldcoinservice.com/procoin/swagger/index.html#/
+        '/procoin-market': {
+          target: 'http://market.encryptedex.com',
+          changeOrigin: true,
+        },
+        '/procoin-file': {
+          target: 'http://upload.encryptedex.com',
+          changeOrigin: true,
+        },
+        '/procoin': {
+          target: 'http://api.encryptedex.com',
+          changeOrigin: true,
+        },
       },
     },
-  },
-  plugins: [
-    vitePluginImp({
-      libList: [
-        {
-          libName: 'antd-mobile',
-          libDirectory: 'es/components',
-          style: () => false,
+    plugins: [
+      vitePluginImp({
+        libList: [
+          {
+            libName: 'antd-mobile',
+            libDirectory: 'es/components',
+            style: () => false,
+          },
+        ],
+      }),
+      react({
+        babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] },
+      }),
+      legacy({
+        targets: {
+          chrome: '49',
+          ios: '10',
         },
-      ],
-    }),
-    react({
-      babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] },
-    }),
-    legacy({
-      targets: {
-        chrome: '49',
-        ios: '10',
-      },
-    }),
-    svgr(),
-  ],
+      }),
+      svgr(),
+    ],
+  };
 });
