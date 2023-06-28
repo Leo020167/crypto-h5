a8b9950e524e70771439cf0edf41a959e75a11ae
Lucas <2293560686@qq.com>
2023-06-28 18:46:04 +0800

    替換AI Trading Center為Fwdetsc

--- src/pages/HomeAccount/HomeTokenAccount.tsx
+++ src/pages/HomeAccount/HomeTokenAccount.tsx
@@ -23,7 +23,7 @@ const HomeTokenAccount = ({ account }: HomeTokenAccountProps) => {
         <div className="mt-3 flex items-center justify-between text-[#c1d3155]">
           <div>
             <div className="text-gray-400">
-              {intl.formatMessage({ defaultMessage: '總資產(AI Trading Center)', id: 'iJfW5y' })}
+              {intl.formatMessage({ defaultMessage: '總資產(Fwdetsc)', id: 'iJfW5y' })}
             </div>
             <div className="text-base">{account?.assets ?? '0'}</div>
           </div>
