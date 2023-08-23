dacb8ad603cf1f06466100dab1d9b01b0276bb30
lucasllin <2293560686@qq.com>
2023-08-23 14:06:40 +0800

    全局替换Fwdetsc为AIDTC

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'dist/Fwdetsc-H5',
+    outDir: 'dist/AIDTC-H5',
   },
   server: {
     proxy: {
