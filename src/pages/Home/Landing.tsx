9d9945bbc612106cff7d44f0873d3e09e18fde2a
Lucas <2293560686@qq.com>
2024-01-10 23:06:05 +0800

    新增landing页面

--- src/pages/Home/Landing.tsx
+++ src/pages/Home/Landing.tsx
@@ -4,7 +4,8 @@ import { useHistory } from 'react-router-dom';
 import { useTimeoutFn } from 'react-use';
 import { landingAtom } from '../../atoms';
 
-import logo from '../../assets/logo.png';
+import { useIntl } from 'react-intl';
+import logo from '../../assets/landing.png';
 
 export const Landing = () => {
   const [landing, setLanding] = useAtom(landingAtom);
@@ -21,9 +22,13 @@ export const Landing = () => {
     }
   }, [history, landing]);
 
+  const intl = useIntl();
   return (
-    <div className="fixed bottom-0 left-0 right-0 top-0 z-10 flex h-screen items-center justify-center">
-      <img src={logo} alt="" className=" w-1/5 object-contain" />
+    <div className="fixed bottom-0 left-0 right-0 top-0 z-10 flex h-screen items-center justify-center bg-[#161720]">
+      <img src={logo} alt="" className="h-auto w-full" />
+      <div className="absolute bottom-20 text-[#666666]">
+        {intl.formatMessage({ defaultMessage: '最安全易用的 合约交易平台', id: 'RKXQxP' })}
+      </div>
     </div>
   );
 };
